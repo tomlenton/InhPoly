2f5ed16a8942c832be3e58dadae350944e7e0b82
tomlenton <twlenton@gmail.com>
2026-03-17 11:16:26 +0000

    Created new Classes from Vehicle

---
+++ Vehicles/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    public abstract class Vehicle
+    {
+        string Make { get; set; }
+        string Model { get; set; }
+        int Speed { get; set; }
+
+        //public Vehicle(string make, string model, int speed)
+        //{
+        //    Make = make;
+        //    Model = model;
+        //    Speed = speed;
+        //}
+    }
+}
